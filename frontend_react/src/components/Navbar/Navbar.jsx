6df0d0309d3943eda28e85637491fac5f481c75f
MustafaPaigeer <mustafapaigeer@live.com>
2022-06-02 19:40:13 -0700

    add animation to navbar with motion frame

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.scss';
 import { images } from '../../constants';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
+  const [toggle, settoggle] = useState(false);
   return (
     <nav className='app__navbar'>
-    <div className='app__navbar-logo'>
-      <img src={images.logo} alt="logo" />
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt="logo" />
       </div>
       <ul className='app__navbar-links'>
-        {['home', 'about', 'work', 'skills', 'contact'].map((item)=> (
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
           <li className='app__flex p-text' key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
+
+      <div className='app__navbar-menue'>
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}>
+            <HiX onClick={() => settoggle(false)} />
+            {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+              <li key={item}>
+                <a href={`#${item}`} onClick={() => settoggle(false)}>{item}</a>
+              </li>
+            ))}
+
+          </motion.div>
+        )}
+      </div>
     </nav>
   )
 }

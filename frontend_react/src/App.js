921c3a0c66279b9c92067b5da00a73872f38fe53
MustafaPaigeer <mustafapaigeer@live.com>
2022-06-06 17:14:13 -0700

    import containers

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-import { About, Footer, Header, Skills, Testimonial, Work} from './container';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+
+    </div>
   )
 }
 

edc503c30012e78f406dfe1d7d30be4131494019
MustafaPaigeer <mustafapaigeer@live.com>
2022-08-02 17:10:13 -0700

    add contact schema

--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -6,6 +6,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import testimonials from './testimonials'
 import abouts from './abouts'
 import brands from './brands'
+import contact from './contact'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -17,6 +18,7 @@ export default createSchema({
     /* Your types here! */
     testimonials,
     abouts,
-    brands
+    brands,
+    contact
   ]),
 })

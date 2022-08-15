018bd44209497d0a6d6e86a5c849f703cbcab6b0
MustafaPaigeer <mustafapaigeer@live.com>
2022-08-14 21:21:23 -0700

    add email and phone to contact section

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -7,7 +7,20 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <h2 className='head-text'>Take a coffee & chat with me </h2>
+
+      <div className='app__footer-cards'>
+        <div className='app__footer-card'>
+        <img src={images.email} alt='email' />
+        <a href='mailto:hello@mustafap.com' className='p-text'>hello@mustafap.com</a>
+        </div>
+        <div className='app__footer-card'>
+        <img src={images.mobile} alt='mobile' />
+        <a href='tel:+1 (236) 886-9744' className='p-text'>+1 (236) 886-9744</a>
+        </div>
+      </div>
+    </>
   )
 }
 

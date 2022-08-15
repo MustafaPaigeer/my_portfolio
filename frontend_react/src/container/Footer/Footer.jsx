03a5642c925eddb3289ee8059181c1d730e3e567
MustafaPaigeer <mustafapaigeer@live.com>
2022-08-15 14:10:13 -0700

    add conditional rendering for sumbit button and correct typos

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -6,25 +6,33 @@ import { client } from '../../client';
 import './Footer.scss';
 
 const Footer = () => {
-const [formData, setFormData] = useState({ name: '', email: ''});
-const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({ name: '', email: '' });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-const { username, email, message } = formData;
+  const { username, email, message } = formData;
 
-const handleChangeInput = (e) => {
-  const { name, value } = e.target;
-  setFormData({ ...formData, [name]: value });
-};
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-const handleSubmit = () => {
-  setLoading(true);
+  const handleSubmit = () => {
+    setLoading(true);
 
-  const contact = {
-    _type: 'contact',
-    name: formData.username,
-    email: formData.email,
-    message: formData.message,
+    const contact = {
+      _type: 'contact',
+      name: formData.username,
+      email: formData.email,
+      message: formData.message,
+    };
+
+    client.create(contact)
+      .then(() => {
+        setLoading(false);
+        setIsFormSubmitted(true);
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -33,36 +41,44 @@ const handleSubmit = () => {
 
       <div className='app__footer-cards'>
         <div className='app__footer-card'>
-        <img src={images.email} alt='email' />
-        <a href='mailto:hello@mustafap.com' className='p-text'>hello@mustafap.com</a>
+          <img src={images.email} alt='email' />
+          <a href='mailto:hello@mustafap.com' className='p-text'>hello@mustafap.com</a>
         </div>
         <div className='app__footer-card'>
-        <img src={images.mobile} alt='mobile' />
-        <a href='tel:+1 (236) 886-9744' className='p-text'>+1 (236) 886-9744</a>
+          <img src={images.mobile} alt='mobile' />
+          <a href='tel:+1 (236) 886-9744' className='p-text'>+1 (236) 886-9744</a>
         </div>
       </div>
 
-      <div className='app__footer-form app__flex'>
-        <div className='app__flex'>
-          <input className='p-text' placeholder='Your Name' name='name' value={name} onChange={hangleChangeInput} />
-        </div>
-        <div className='app__flex'>
-          <input className='p-text' placeholder='Your Email' name='email' value={email} onChange={hangleChangeInput} />
-        </div>
-        <div>
-          <textarea
-            className='p-text'
-            placeholder='Your Message'
-            value={message}
-            name={message}
-            onChange={handleChangeInput}
-          />
+      {!isFormSubmitted ? (
+        <div className='app__footer-form app__flex'>
+          <div className='app__flex'>
+            <input className='p-text' placeholder='Your Name' name='name' value={username} onChange={handleChangeInput} />
+          </div>
+          <div className='app__flex'>
+            <input className='p-text' placeholder='Your Email' name='email' value={email} onChange={handleChangeInput} />
+          </div>
+          <div>
+            <textarea
+              className='p-text'
+              placeholder='Your Message'
+              value={message}
+              name={message}
+              onChange={handleChangeInput}
+            />
+          </div>
           <button type='button' className='p-text' onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
         </div>
-      </div>
+      ) : (
+        <div>
+          <h3 className="head-text">
+            Thank you for getting in touch!
+          </h3>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default AppWrap(
   MotionWrap(Footer, 'app__footer'),

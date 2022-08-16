d964d8459364693e361cb53abe72bb60b9d535d7
MustafaPaigeer <mustafapaigeer@live.com>
2022-08-16 15:24:58 -0700

    change the social media icons

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsTwitter, BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => {
@@ -8,10 +8,10 @@ const SocialMedia = () => {
         <div className="app__social">
 
             <div>
-                <BsTwitter />
+                <BsLinkedin />
             </div>
             <div>
-                <FaFacebookF />
+                <BsGithub />
             </div>
             <div>
                 <BsInstagram />
